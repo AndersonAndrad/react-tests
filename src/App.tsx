6bd62746b3ffadc749d9eb977ed1ab733ffb68c8
AndersonAndrad <anderson_andrade_@outlook.com>
2022-10-02 22:06:10 -0300

    create history component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css'
 import { Flex } from '@chakra-ui/react'
 import History from './components/history/history'
 import SideMenu from './components/side-menu/side-menu'
+import Tools from './components/tools/tools'
 
 function App() {
   return (
     <Flex>
       <SideMenu />
-      <History />
+      <Flex bg='gray.900' direction='column'>
+        <History />
+        <Tools />
+      </Flex>
     </Flex>
   )
 }
